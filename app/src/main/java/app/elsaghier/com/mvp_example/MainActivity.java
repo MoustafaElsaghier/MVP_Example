1966a9c9cb7fb9fbe9753fd022587fdd91690d24
EL_Saghier <thedreamereng@gmail.com>
2019-07-19 17:14:28 +0200

    init login code

--- app/src/main/java/app/elsaghier/com/mvp_example/MainActivity.java
+++ app/src/main/java/app/elsaghier/com/mvp_example/MainActivity.java
@@ -2,12 +2,31 @@ package app.elsaghier.com.mvp_example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText email, pass;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email = findViewById(R.id.email_tv);
+        pass = findViewById(R.id.pass_tv);
+        login = findViewById(R.id.login_btn);
+        login.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.login_btn) {
+            String emailText = email.getText().toString();
+            String passText = pass.getText().toString();
+        }
     }
 }
