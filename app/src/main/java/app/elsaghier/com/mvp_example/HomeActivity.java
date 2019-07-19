7736e2e33605b93157865294c5ef16a9012fd49c
EL_Saghier <thedreamereng@gmail.com>
2019-07-19 17:07:34 +0200

    create design for login screen

--- app/src/main/java/app/elsaghier/com/mvp_example/HomeActivity.java
+++ app/src/main/java/app/elsaghier/com/mvp_example/HomeActivity.java
@@ -2,12 +2,16 @@ package app.elsaghier.com.mvp_example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+    TextView userName, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        userName = findViewById(R.id.user_name_tv);
+        password = findViewById(R.id.pass_tv);
     }
 }
